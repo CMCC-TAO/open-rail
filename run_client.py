a7876f2ce59442db3179dba1edf649cec6c20550
zhaoyongsheng <zhaoyongsheng@cmhi.chinamobile.com>
2025-04-21 15:24:39 +0800

    OCT add keyboard control

--- run_client.py
+++ run_client.py
@@ -10,13 +10,23 @@ if __name__ == "__main__":
     robot = RobotA2D(config.observer, config.controller)
     rdm = RealtimeDataManager(config.rdm)
     vla_client = VLAClient(config, rdm, robot)
-    vla_client.startObserve()
     
     try:
         while True:
-            time.sleep(1)
+            cmd = input("请输入指令：")
+            # print(f'输入的指令是：{cmd}')
+            if cmd == 'exit':
+                break
+            elif cmd == 'start' or cmd == 'run':
+                vla_client.startObserve()
+            elif cmd == 'stop':
+                print("停止")
+            else:
+                print('未知指令')
+            time.sleep(0.1)
     except KeyboardInterrupt:
         print("程序被中断")
     finally:
-        robot.close()
-        vla_client.close()
+        pass
+        # robot.close()
+        # vla_client.close()
