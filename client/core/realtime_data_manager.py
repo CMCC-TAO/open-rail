6f3f463b83ebb72eb7175c407033fe1b368f2482
zhaoyongsheng <zhaoyongsheng@cmhi.chinamobile.com>
2025-04-21 08:53:24 +0800

    OCT update realtime data manager

--- client/core/realtime_data_manager.py
+++ client/core/realtime_data_manager.py
@@ -4,11 +4,20 @@ class RealtimeDataManager():
     def __init__(self, rdm_config: ConfigDict):
         self.rdm_config = rdm_config
         self.observe_buffer = deque(maxlen=rdm_config.max_len)
+        self.control_buffer = []
         if rdm_config.record_data:
             pass #
 
+        if rdm_config.show_data:
+            pass #
+
+        self.currt_timestamp = None
+
+
     def add(self, msg):
-        self.buffer.append(msg)
+        # 将传入的消息msg添加到buffer列表中
+        self.observe_buffer.append(msg)
+        self.observe_buffer.pop()
 
     def get_closest(self, target_stamp):
         # 找到时间最接近的消息
