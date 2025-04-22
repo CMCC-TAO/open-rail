8dda02af2debc137f1645d03ea42ecc19d6ecb92
zhaoyongsheng <zhaoyongsheng@cmhi.chinamobile.com>
2025-04-22 08:59:36 +0800

    OCT commit before infer api

--- client/core/vla_client.py
+++ client/core/vla_client.py
@@ -48,6 +48,37 @@ class BaseClient():
         #             self.inference_thread()
         #         else:
         #             print("未设置inference_thread")
+    def receive_messages(self):
+        try:
+            # 阻塞接收，在线程中使用
+            if self.dealer.poll() != 0:
+                message = {}
+                parts = self.dealer.recv_multipart() # 接收多部分消息
+                if len(parts) >= 2:
+                    message['data'] = pickle.loads(parts[0]) # 0是二进制数据
+                    message['meta'] = json.loads(parts[1].decode('utf8')) # 1是元数据JSON
+                    return message
+
+                    # with self.result_lock:
+                    #     if self.receive_callback:
+                    #         self.receive_callback(message_obj)
+                    #     else:
+                    #         print(f"收到消息，但未设置回调处理。msg: {message_obj}")
+        except Exception as e:
+            print(f"接收消息时出错: {e}")
+            import traceback
+            traceback.print_exc()
+    
+    def send_message(self, data, meta={}):
+        try:
+            # 图像send前需编码：_, img_encoded = cv2.imencode('.jpg', data), 收到需要解码: img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            data = pickle.dumps(data) # data字典转为字节流
+            meta = json.dumps(meta).encode('utf8')
+            self.dealer.send_multipart([data, meta], flags=zmq.NOBLOCK) # 非阻塞发送
+        except Exception as e:
+            print(f"发送消息时出错: {e}")
+            import traceback
+            traceback.print_exc()
 
     def control_thread_fun(self):
         pass
