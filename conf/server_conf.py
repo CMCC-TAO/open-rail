6a176d027eb9b832d5fdcda205c3db77d9dff582
wenxuanzhang <wenxuanzhang@cmhi.chinamobile.com>
2025-07-10 10:12:49 +0800

    OCT merge master

--- conf/server_conf.py
+++ conf/server_conf.py
@@ -13,12 +13,5 @@ def get_server_config():
     config.zmq = get_zmq_config()
     # MAIN_CLIENT_ID = 'ZROBOT'
     config.max_workers = 1  # 推理线程池最大工作线程数，1表示不支持并发推理
-<<<<<<< HEAD
-    # config.model = ModelType.ACT
-    # config.model = ModelType.SMOLVLA
-    config.model = ModelType.GR00T_N1_5
-    return config
-=======
     config.model = get_model_config()
     return config
->>>>>>> master
