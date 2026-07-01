0ab9d4684e03650ba2da82f7f54085405683e8da
zhaoyongsheng <zhaoyongsheng@cmhi.chinamobile.com>
2026-07-01 21:31:05 +0800

    Add HROBOT-14238 fix inference first timeout problem and add timeout config in vla_zmq

--- conf/zmq_conf.py
+++ conf/zmq_conf.py
@@ -16,6 +16,7 @@ def get_vla_zmq_config():
     # config.server_addr = 'tcp://*:5566'  # Server binding address and port
     config.ip = '127.0.0.1'  # Client connection address
     config.port = '5566'     # Client connect port
+    config.timeout = 500     # Timeout for zmq.recv(), milliseconds
     # config.server_ip = '*'          # Server binding address
     # config.server_port = '5566'     # Server binding port
     return config
