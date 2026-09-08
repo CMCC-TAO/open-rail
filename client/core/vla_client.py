10023c6574e0d2674e2b1432547a733a49a65bc4
zhaoyongsheng <zhaoyongsheng@cmhi.chinamobile.com>
2026-09-08 17:35:15 +0800

    Add HROBOT-14238 remove image copy for a2d robot

--- client/core/vla_client.py
+++ client/core/vla_client.py
@@ -390,7 +390,7 @@ class VLAClient():
                 time.sleep(0.1)
                 continue
             else:
-                time.sleep(0.005/retrieve_try_num)
+                time.sleep(0.01/retrieve_try_num)
                 retrieve_try_num = retrieve_try_num + 1.0
 
             observations = self.robot.retrieve_observation()
